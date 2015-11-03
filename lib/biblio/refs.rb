254c79c0cd9f3208fc03c2dc3740308909cf94d6
Jonathan Expósito <alu0100696455@ull.edu.es>
2015-11-03 17:01:36 +0000

    Desarrollado código para cumplir la expectativa de existencia de editorial

--- lib/biblio/refs.rb
+++ lib/biblio/refs.rb
@@ -3,12 +3,13 @@ require "biblio/refs/version"
 module Biblio
   module Refs
     class Referencia
-    	attr_accessor :autores, :titulo, :serie
+    	attr_accessor :autores, :titulo, :serie, :editorial
 
-    	def initialize(autores, titulo, serie = nil)
+    	def initialize(autores, titulo, serie = nil, editorial)
     		@autores = autores
     		@titulo = titulo
     		@serie = serie
+    		@editorial = editorial
     	end
     end
   end
