e948b2d04dd53279a49465b665ad14aab71a082d
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-11-03 16:46:57 +0000

    Añadida expectativa para comprobar la existencia o no de una serie

--- spec/biblio/refs_spec.rb
+++ spec/biblio/refs_spec.rb
@@ -4,7 +4,8 @@ require 'spec_helper'
 describe Biblio::Refs do
 
   before :each do
-    @ref1 = Biblio::Refs::Referencia.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide")
+    @ref1 = Biblio::Refs::Referencia.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide", "The Facets of Ruby")
+    @ref2 = Biblio::Refs::Referencia.new("J.R.R Tolkien", "The Hobbit")
   end
 
   it 'Tiene un número de versión' do
@@ -17,5 +18,10 @@ describe Biblio::Refs do
 
   it 'Debe de existir un título' do
     expect(@ref1.titulo).not_to be nil
+  end
+
+  it 'Debe o no existir una serie' do
+    expect(@ref1.serie).not_to be nil
+    expect(@ref2.serie).to_be nil
   end
 end
