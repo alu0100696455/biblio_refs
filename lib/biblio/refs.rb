516b3d42307461381896f21b97e8a35e70a1564a
Jonathan Expósito <alu0100696455@ull.edu.es>
2015-11-03 17:09:02 +0000

    Desarrollado código para cumplir la expectativa de existencia de número de edición

--- lib/biblio/refs.rb
+++ lib/biblio/refs.rb
@@ -3,13 +3,14 @@ require "biblio/refs/version"
 module Biblio
   module Refs
     class Referencia
-    	attr_accessor :autores, :titulo, :serie, :editorial
+    	attr_accessor :autores, :titulo, :serie, :editorial, :num_edicion
 
-    	def initialize(autores, titulo, serie = nil, editorial)
+    	def initialize(autores, titulo, serie = nil, editorial, num_edicion)
     		@autores = autores
     		@titulo = titulo
     		@serie = serie
     		@editorial = editorial
+    		@num_edicion = num_edicion
     	end
     end
   end
