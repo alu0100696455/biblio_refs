b36707b405da463c096bed19266d1d5b0ab6d7e0
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-11-03 16:38:41 +0000

    Añadida expectativa para la existencia de un título

--- spec/biblio/refs_spec.rb
+++ spec/biblio/refs_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
+
 describe Biblio::Refs do
 
   before :each do
+    @ref1 = Biblio::Refs::Referencia.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide")
   end
 
   it 'Tiene un número de versión' do
     expect(Biblio::Refs::VERSION).not_to be nil
   end
 
-  it 'Debe de existir uno o más autores' do
+  it 'Deben de existir uno o más autores' do
     expect(@ref1.autores).not_to be nil
+  end
+
+  it 'Debe de existir un título' do
+    expect(@ref1.titulo).not_to be nil
   end
 end
