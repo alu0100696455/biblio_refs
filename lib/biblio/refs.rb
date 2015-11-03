cd024664a162c02251740f852a18caac23b5ec0d
Jonathan Expósito <alu0100696455@ull.edu.es>
2015-11-03 17:20:33 +0000

    Desarrollado código para cumplir la expectativa de existencia de fecha de publicación

--- lib/biblio/refs.rb
+++ lib/biblio/refs.rb
@@ -1,16 +1,18 @@
 require "biblio/refs/version"
+require 'date'
 
 module Biblio
   module Refs
     class Referencia
-    	attr_accessor :autores, :titulo, :serie, :editorial, :num_edicion
+    	attr_accessor :autores, :titulo, :serie, :editorial, :num_edicion, :fecha_publicacion
 
-    	def initialize(autores, titulo, serie = nil, editorial, num_edicion)
+    	def initialize(autores, titulo, serie = nil, editorial, num_edicion, fecha_publicacion)
     		@autores = autores
     		@titulo = titulo
     		@serie = serie
     		@editorial = editorial
     		@num_edicion = num_edicion
+    		@fecha_publicacion = fecha_publicacion
     	end
     end
   end
