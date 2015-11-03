42d5a3a475562be3f7cc77a287ecc215cb4b5347
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-11-03 15:52:05 +0000

    Añadida expectativa para la existencia de autores

--- spec/biblio/refs_spec.rb
+++ spec/biblio/refs_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 
 describe Biblio::Refs do
-  it 'has a version number' do
+
+  before :each do
+  end
+
+  it 'Tiene un número de versión' do
     expect(Biblio::Refs::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'Debe de existir uno o más autores' do
+    expect(@ref1.autores).not_to be nil
   end
 end
